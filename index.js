0fdff62e3d81b606e01424d9a3b8c2ed3de1b5e7
RichardBray <richardbray89@gmail.com>
2024-01-25 16:41:18 +0000

    chore: remove cloudinary related code

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
-import { v2 as cloudinary } from 'cloudinary';
 import nodemailer from 'nodemailer';
 import data from './data.json' assert { type: 'json' };
-
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
 
 const transporter = nodemailer.createTransport({
   host: process.env.NODEMAILER_HOST,
@@ -28,46 +21,16 @@ const personalisedText = {
 function main() {
   return data.map(async (user) => {
     try {
-      const generatedImage = await cloudinary.image(`email-${user.interest}`, {
-        transformation: [
-          { width: 800, height: 500, crop: 'fill', format: 'jpg', radius: 10 },
-          {
-            overlay: {
-              font_family: 'Arial',
-              font_size: 50,
-              font_weight: 'bold',
-              text: personalisedText[user.interest],
-            }, color: "#fff", background: "#00000030",
-          },
-          { flags: "layer_apply", gravity: "north_east", y: 50, x: 115 },
-          { overlay: `email-overlay-${user.interest}` },
-          { width: 315, height: 250, crop: 'fill' },
-          { flags: "layer_apply", gravity: "south_east", y: 80, x: 80, border: "5px_solid_white" },
-          {
-            overlay: {
-              font_family: 'Arial',
-              font_size: 30,
-              font_weight: 'bold',
-              text: 'VIEW STORE',
-            }, color: "#fff", background: "black",
-          },
-          { flags: "layer_apply", gravity: "south_east", y: 20, x: 200, border: "5px_solid_black" },
-        ]
-      });
-
-      const email = await transporter.sendMail({
+      await transporter.sendMail({
         from: process.env.NODEMAILER_USER,
         to: process.env.EMAIL_RECIPIENT,
         subject: personalisedText[user.interest],
-        html: `<h1>Hello ${user.firstName} we have something you might like</h1> <a href="#">${generatedImage}</a><p>We know you like ${user.interest} so we thought you might like this</p>`,
+        html: `<h1>Hello ${user.firstName} we have something you might like</h1>`,
       });
-
-      console.log("Email response:", email.response);
     } catch (error) {
       console.error(error);
     }
   });
-
 }
 
 await Promise.all(main());
